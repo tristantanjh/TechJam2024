f1a00b87faa0ca2977bdcc0225360e9370f05b44
tris <tristantanjh@gmail.com>
2024-06-17 19:15:37 +0800

    complete web scraper

--- notebooks/scraper.py
+++ notebooks/scraper.py
@@ -1,5 +1,6 @@
 import requests
 import os
+import time
 from bs4 import BeautifulSoup as soup
 from dotenv import load_dotenv
 from langchain_community.graphs import Neo4jGraph
@@ -7,6 +8,10 @@ from langchain_experimental.graph_transformers import LLMGraphTransformer
 from langchain_openai import ChatOpenAI
 from langchain_text_splitters import TokenTextSplitter
 from langchain_core.documents import Document
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
 
 load_dotenv()
 
@@ -36,13 +41,23 @@ def process_and_store_text(clean_text, llm_transformer, graph):
     graph_documents = llm_transformer.convert_to_graph_documents(documents)
     graph.add_graph_documents(graph_documents, baseEntityLabel=True, include_source=True)
     
-def scrape_tiktok_gettingstarted():
+def scrape_all_categories():
+    # all help center categories - use when scraping all categories after upgrading to a paid plan
+    tt_urls = ["https://ads.tiktok.com/help/category?id=5pc1e9Q09towekjAqXm8b1", "https://ads.tiktok.com/help/category?id=4vtMvaC8OcjlLiL20rm434", 
+               "https://ads.tiktok.com/help/category?id=79iQ84FDPgWbtbDy1zeRmg", "https://ads.tiktok.com/help/category?id=4lc0aCQeinkF936Ulb3Tiw", 
+               "https://ads.tiktok.com/help/category?id=72Zi2CfIYTaDuOwzHsFf5f", "https://ads.tiktok.com/help/category?id=6dGs4bNMAZSdPr4pQ0KFuX"]
+    
+    tt_base_url = 'https://ads.tiktok.com'
+    
+    for tt_url in tt_urls:
+        scrape_category(tt_url, tt_base_url)
+    
+def scrape_category(tt_url, tt_base_url):
     """
     Scrape Tiktok Business Center Getting Started Page
     """   
-    # Base URL
-    tt_url="https://ads.tiktok.com/help/category?id=5pc1e9Q09towekjAqXm8b1"
-    tt_base_url = 'https://ads.tiktok.com'
+    # Set up Selenium WebDriver
+    driver = webdriver.Chrome()
 
     html = requests.get(tt_url)
 
@@ -51,34 +66,37 @@ def scrape_tiktok_gettingstarted():
 
     links = bsobj.findAll("a", {"class": "category_card_catalog_item"})
     
-    names = [link.text for link in links]
-    # print(names)
     urls = [tt_base_url + link.get('href') for link in links]
-    # print(urls)
     
-    print("Scraping category page:", urls[0])
-    scrape_category_page(tt_base_url, urls[0])
+    for url in urls:
+        print("Scraping category page:", url)
+        scrape_category_page(tt_base_url, url, driver)
     
     # later when calling process_and_store_text, try initializing the graph and llm_transformer in the function
+    # can try putting this function in the scrape_article_page function?
     # process_and_store_text(clean_text, llm_transformer, graph)
     
-def scrape_category_page(tt_base_url, url):
+def scrape_category_page(tt_base_url, url, driver):
     """
     Scrape the category page to get all links under ul.bui-menu-ul
     """
-    html = requests.get(url)
-    bsobj = soup(html.content, 'lxml')
+    driver.get(url)
+    time.sleep(5)  # Wait for JavaScript to load the content
+    
+    # Get the page source and parse it with BeautifulSoup
+    page_source = driver.page_source
+    bsobj = soup(page_source, 'lxml')
     
     sidebar = bsobj.find("div", {"class": "category_tree_wrapper"})
-    print(sidebar)
     if sidebar:
-        ul = sidebar.find("ul", {"class": "category_tree_menu"})
-        print(ul)
-        if ul:
-            active_links = ul.findAll("a", href=True)
-            for link in active_links:
-                page_url = tt_base_url + link['href']
-                scrape_article_page(page_url)
+        opened_list = sidebar.find("li", {"class": "bui-submenu-opened"})
+        if opened_list:
+            ul = opened_list.find("ul", {"class": "bui-menu-ul"})
+            if ul:
+                active_links = ul.findAll("a", href=True)
+                for link in active_links:
+                    page_url = tt_base_url + link['href']
+                    scrape_article_page(page_url)
 
 def scrape_article_page(url):
     """
@@ -95,6 +113,7 @@ def scrape_article_page(url):
         print("Content:", content.text.strip())
         print("-" * 80)
 
-scrape_tiktok_gettingstarted()
+################################ SCRAPE GETTING STARTED PAGE ONLY (FOR TESTING) ################################
+scrape_category("https://ads.tiktok.com/help/category?id=5pc1e9Q09towekjAqXm8b1", "https://ads.tiktok.com")
 
 
