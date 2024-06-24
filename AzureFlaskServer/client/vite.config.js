30085e05dc038c664dc8c8efabd00d5f16c2f79c
oscarqjh <oscar.jh9@gmail.com>
2024-06-24 23:02:17 +0800

    vite config

--- AzureFlaskServer/client/vite.config.js
+++ AzureFlaskServer/client/vite.config.js
@@ -1,15 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
-import { fileURLToPath, URL } from 'node:url'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+import { fileURLToPath, URL } from "node:url";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      // '@': path.resolve(__dirname, './src'),
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      "@": path.resolve(__dirname, "./src"),
+      // "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
-})
+  server: {
+    fs: {
+      cachedChecks: false,
+    },
+  },
+});
