88039330d5e77547d9810244ff1b82e1f3e83454
Zhetao <quzhetao2001@gmail.com>
2024-06-27 21:38:26 +0800

    Minor styling changes

--- AzureFlaskServer/client/src/Pages/TranscriberPage/Components/ai-message.jsx
+++ AzureFlaskServer/client/src/Pages/TranscriberPage/Components/ai-message.jsx
@@ -1,9 +1,10 @@
 export default function AiMessage({ points }) {
   return (
-    <div>
+    <div className='p-2 border-solid border-black border-b-2 border-slate-300'>
       {points.map((point, idx) => (
-        <p key={idx}>- {point}</p>
+        <p key={idx} className='m-2'>- {point}</p>
       ))}
+      {/* <hr/> */}
     </div>
   );
 }
--- AzureFlaskServer/client/src/Pages/TranscriberPage/Components/llm-output.jsx
+++ AzureFlaskServer/client/src/Pages/TranscriberPage/Components/llm-output.jsx
@@ -3,6 +3,9 @@ import AiMessage from "./ai-message";
 export default function LLMOutput({ aiMessages }) {
   return (
     <div>
+      <h3 className="p-3 flex justify-center scroll-m-20 border-t border-b text-2xl font-semibold tracking-tight">
+        LLM Output
+      </h3>
       {aiMessages?.map((item, idx) => (
         <AiMessage key={idx} points={item.split("-").slice(1)} />
       ))}
