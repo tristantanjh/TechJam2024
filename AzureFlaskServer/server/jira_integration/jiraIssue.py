ee514547d8691356b9684221238c67985b6ac152
joshuagohez <jgoh.ezhong@gmail.com>
2024-07-07 00:14:38 +0800

    dynamic jira issue API call + adjust transcriber call

--- AzureFlaskServer/server/jira_integration/jiraIssue.py
+++ AzureFlaskServer/server/jira_integration/jiraIssue.py
@@ -1,60 +1,8 @@
 import requests
 from requests.auth import HTTPBasicAuth
 import json
-import os
-from dotenv import load_dotenv
 
-def create_issue(summary, description):
-    load_dotenv()
-
-    url = "https://2waffles.atlassian.net/rest/api/3/issue"
-    
-    auth = HTTPBasicAuth("joshuagohengzhong@gmail.com", os.getenv("JIRA_API_KEY"))
-    
-    headers = {
-        "Accept": "application/json",
-        "Content-Type": "application/json"
-    }
-    
-    payload = json.dumps({
-        "fields": {
-            "description": {
-                "content": [
-                    {
-                        "content": [
-                            {
-                                "text": description,
-                                "type": "text"
-                            }
-                        ],
-                        "type": "paragraph"
-                    }
-                ],
-                "type": "doc",
-                "version": 1
-            },
-            "summary": summary,
-            "issuetype": {
-                "id": "10001"
-            },
-            "project": {
-                "id": "10000"
-            },
-        },
-    })
-    
-    response = requests.request(
-        "POST",
-        url,
-        data=payload,
-        headers=headers,
-        auth=auth
-    )
-    
-    return json.dumps(json.loads(response.text), sort_keys=True, indent=4, separators=(",", ": "))
-
-
-def create_issue_copilot(endpoint, title, description, auth):
+def create_issue(endpoint, title, description, auth):
 
     url = f"{endpoint}/rest/api/3/issue"
     authKey = list(auth[0].keys())[0]
