c6ce28e3dbb0ca951e3d92d8f0e8b6c8fb9e53c8
oscarqjh <oscar.jh9@gmail.com>
2024-07-07 02:50:02 +0800

    error page done

--- AzureFlaskServer/client/src/Pages/ErrorPage/ErrorPage.jsx
+++ AzureFlaskServer/client/src/Pages/ErrorPage/ErrorPage.jsx
@@ -86,7 +86,10 @@ export default function ErrorPage() {
           transition={{ duration: 1, ease: "easeInOut", delay: 0.6 }}
           className="absolute z-[999] h-screen w-screen flex justify-center items-center"
         >
-          <Button className="inline-flex h-12 animate-shimmer items-center justify-center rounded-full border border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-6 font-medium text-slate-200 transition-all focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50 hover:text-slate-500">
+          <Button
+            className="inline-flex h-12 animate-shimmer items-center justify-center rounded-full border border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-6 font-medium text-slate-200 transition-all focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50 hover:text-slate-500"
+            asChild
+          >
             <Link to="/app">Go Back</Link>
           </Button>
         </motion.div>
