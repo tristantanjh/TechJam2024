840c24e43782bdafd2d8306f5473cb430d3687b3
oscarqjh <oscar.jh9@gmail.com>
2024-07-06 23:50:47 +0800

    .

--- AzureFlaskServer/server/jira_integration/jiraIssue.py
+++ AzureFlaskServer/server/jira_integration/jiraIssue.py
@@ -52,3 +52,53 @@ def create_issue(summary, description):
     )
     
     return json.dumps(json.loads(response.text), sort_keys=True, indent=4, separators=(",", ": "))
+
+
+def create_issue_copilot(endpoint, title, description, type, priority, auth):
+
+    url = f"{endpoint}/rest/api/3/issue"
+    authHead = auth[0]
+    
+    auth = HTTPBasicAuth("joshuagohengzhong@gmail.com", os.getenv("JIRA_API_KEY"))
+    
+    headers = {
+        "Accept": "application/json",
+        "Content-Type": "application/json"
+    }
+    
+    payload = json.dumps({
+        "fields": {
+            "description": {
+                "content": [
+                    {
+                        "content": [
+                            {
+                                "text": description,
+                                "type": "text"
+                            }
+                        ],
+                        "type": "paragraph"
+                    }
+                ],
+                "type": "doc",
+                "version": 1
+            },
+            "summary": summary,
+            "issuetype": {
+                "id": "10001"
+            },
+            "project": {
+                "id": "10000"
+            },
+        },
+    })
+    
+    response = requests.request(
+        "POST",
+        url,
+        data=payload,
+        headers=headers,
+        auth=auth
+    )
+    
+    return json.dumps(json.loads(response.text), sort_keys=True, indent=4, separators=(",", ": "))
